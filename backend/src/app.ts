5da5fc0f3e56c710a53b1576b5eaf4a1142290e4
Arijit Mondal <arijitm717@gmail.com>
2025-12-25 18:44:09 +0530

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.routes";
 import postRouter from "./routes/post.routes";
+import chatRouter from "./routes/chat.routes";
 
 const app = express();
 
+// middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
@@ -16,7 +18,9 @@ app.use(
   })
 );
 
+// routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/chats", chatRouter);
 
 export default app;
